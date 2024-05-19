6e28d900e95ae7f27b72be7a9aac9fcb6c9b22b5
XiangcenWu <93538241+XiangcenWu@users.noreply.github.com>
2024-05-19 14:52:40 +0100

    change gitignore

--- GUI/gui.cpp
+++ GUI/gui.cpp
@@ -1,16 +1,13 @@
 #include "MainWindow.h"
 #include "ImageLoader.h"
-#include <QStyleFactory>
 #include <iostream>
 
 
 int main(int argc, char *argv[])
 {
-
     std::cout << test_image << std::endl;
     QApplication a(argc, argv);
     MainWindow w;
-    w.setStyleSheet("QWidget { background-color: white; }");
     w.show();
     return a.exec();
 }
